c8f14e30e98026228bacc1ec773fdea7108f30dd
Akash <ajcodes.tech@gmail.com>
2026-01-14 21:40:56 +0530

    k frequent element

---
+++ src/main/kotlin/io/github/ajcode404/taro/KFrequentElements.kt
@@ -0,0 +1,32 @@
+package io.github.ajcode404.taro
+
+    import java.util.PriorityQueue
+
+data class Ele(
+    val frequency: Int,
+    val number: Int
+)
+class KFrequentElements {
+    val countMap = mutableMapOf<Int, Int>()
+    val priorityQueue = PriorityQueue<Ele>(compareByDescending { it.frequency })
+
+    fun topKFrequent(nums: IntArray, k: Int): IntArray {
+        nums.forEach {
+            countMap[it] = (countMap[it] ?: 0) + 1
+        }
+        for (entry in countMap) {
+            priorityQueue.add(Ele(entry.value, entry.key))
+        }
+        val arr = IntArray(k)
+        for (i in 0..k - 1) {
+            arr[i] = priorityQueue.poll().number
+        }
+        return arr
+    }
+}
+
+fun main() {
+    val nums = intArrayOf(1,1,1,2,2,3)
+    val k = 2
+    KFrequentElements().topKFrequent(nums, k).forEach { it.print() }
+}
